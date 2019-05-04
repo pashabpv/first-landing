dc18a52daa16465af6b813ba82ee6d6608c99a12
pashabpv <pasha.bpv@gmail.com>
2019-05-04 15:47:57 +0300

    edit section 1

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
 	// Custom JS
-
+	
+	// Menu Burger
+	var menuBurger = $('.menuBurgerAnimate');
+	menuBurger.on('click' , function () {
+			menuBurger.toggleClass('active');
+	});
+	// End Menu Burger
 });
